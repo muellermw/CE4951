1ffb0efc2101b7c11bb08a15e0abfe858b59faf9
Mueller <muellermw@ad.msoe.edu>
2018-09-18 09:12:55 -0500

    initial commit

---
+++ src/main.c
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_nucleo.h"
+			
+
+int main(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpioA;
+	gpioA.Pin = GPIO_PIN_11 |
+				 GPIO_PIN_10 |
+				 GPIO_PIN_9  |
+				 GPIO_PIN_8  |
+				 GPIO_PIN_7;
+	gpioA.Mode = GPIO_MODE_OUTPUT_PP;
+
+	HAL_GPIO_Init(GPIOA, &gpioA);
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+
+	for(;;);
+}
